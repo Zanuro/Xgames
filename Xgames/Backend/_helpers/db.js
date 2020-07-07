446599b24c9a0cd7c00b54b6e9e4ab40a53e5bb6
Marta García <alu0101015927@ull.edu.es>
2020-07-07 01:11:39 +0100

    arreglando conflictos

--- Xgames/Backend/_helpers/db.js
+++
@@ -1,8 +0,0 @@
-const config = require('configuration.json');
-const mongoose = require('mongoose');
-mongoose.connect(config.connectionString, { useCreateIndex: true, useNewUrlParser: true });
-mongoose.Promise = global.Promise;
-
-module.exports = {
-    User: require('../users/user.model')
-};
